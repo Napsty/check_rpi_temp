250a415695f79e9f1dd46000e07cfa42188700e7
Napsty <ck@claudiokuenzler.com>
2020-02-07 14:10:26 +0100

    Update GPL

--- check_rpi_temp.py
+++ check_rpi_temp.py
@@ -17,11 +17,9 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-# 02110-1301, USA.
+# along with this program; if not, see <https://www.gnu.org/licenses/>.
 #
-# Copyright (c) 2019 Claudio Kuenzler www.claudiokuenzler.com
+# Copyright (c) 2019-2020 Claudio Kuenzler www.claudiokuenzler.com
 # Copyright (c) 2020 Tobias Bossert
 #
 # History:
